8651c93e06b50a1f41847af2d173a88b04650dde
houst0nn <houstonn@gmail.com>
2014-01-19 01:45:48 -0500

    test

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9320; \
         ln -sf /data/misc/audio/wcd9320_anc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin)
+        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin; \
+        ln -sf /data/misc/audio/mbhc.bin \
+        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mbhc.bin)
 
 endif
